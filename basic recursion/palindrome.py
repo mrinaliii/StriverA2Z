2d813f92325ea41c2420da6e3150406031f4c62d
mrinaliii <163443152+mrinaliii@users.noreply.github.com>
2026-02-19 10:21:51 +0530

    Palindrome string

---
+++ basic recursion/palindrome.py
@@ -0,0 +1,9 @@
+def palindrome(s):
+    n = len(s)
+    for i in range(n//2):
+            if s[i]!=s[n-i-1]:
+                return False
+    return True
+
+s="ABCDCBA"
+print("Palindrome") if palindrome(s) else print("Not Palindrome")
